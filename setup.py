bdcf6c84648b0654a7665fa5f787e6a448f8181e
Connor Keenan <ckeenan89@gmail.com>
2015-12-02 12:42:52 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='captureth',
+      version='0.0.1',
+      url='http://github.com/Consensys/pycaptureth/',
+      license='MIT',
+      packages=['captureth'])
