8f367f8ec58affb2dcafdc1094a9e46ef06fb592
Connor Keenan <ckeenan89@gmail.com>
2016-01-26 19:00:59 -0700

    added deps, requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
+install_requires = set(x.strip() for x in open('requirements.txt'))
+
 setup(name='captureth',
-      version='0.0.1',
+      version='0.0.5',
       url='http://github.com/Consensys/pycaptureth/',
+      description='Capture msgs from pyethapp',
+      install_requires=install_requires,
       license='MIT',
       packages=['captureth'])
